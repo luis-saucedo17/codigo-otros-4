f92b46a31fd8f0b347057ff82da57ce67156c82c
Luis Saucedo <luisrubalcava17@gmail.com>
2023-02-15 15:56:39 -0600

    add: comments

--- src/com/generation/Java4.java
+++ src/com/generation/Java4.java
@@ -1,9 +1,11 @@
 package com.generation;
 
+// Importar Scanner
 import java.util.Scanner;
 
 public class Java4 {
     public static void main(String[] args) {
+    	//Faltaba el argumento del método Scanner
         Scanner s = new Scanner(System.in);
         System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
         String j1 = s.nextLine();
@@ -11,8 +13,11 @@ public class Java4 {
         // Turno del jugador 2
         System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
         Scanner s2 = new Scanner(System.in);
+        //No se hacía uso de la variable s2
         String j2 = s2.nextLine();
-
+        
+        // En lugar de hacer la comparación, usar el método equals
+        // Faltaban los break en algunos case
         if (j1.equals(j2)) {
             System.out.println("Empate");
         } else {
@@ -27,8 +32,10 @@ public class Java4 {
                     if (j2.equals("piedra")) {
                         g = 1;
                     }
+                    // No tenia la llave que cerraba el bloque
                     break;
-                case "tijera":
+                // El case debe ser tijeras 
+                case "tijeras":
                     if (j2.equals("papel")) {
                         g = 1;
                     }
